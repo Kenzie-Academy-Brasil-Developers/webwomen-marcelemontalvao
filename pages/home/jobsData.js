58196ef56ae50d2e608c0c8849339acf6e4f020b
marcelemontalvao <mmontalvaoblog@outlook.com>
2022-10-12 21:05:49 -0300

    sticky in aside

--- pages/home/jobsData.js
+++ pages/home/jobsData.js
@@ -1,7 +1,7 @@
 const jobsData = [
   {
     id: 0,
-    title: "Pessoa desenvolvedora front-end - React",
+    title: "Desenvolvedora Front-end - React",
     enterprise: "Kenzie",
     location: "Curitiba",
     description:
@@ -10,7 +10,7 @@ const jobsData = [
   },
   {
     id: 1,
-    title: "Pessoa desenvolvedora back-end - Node JS",
+    title: "Desenvolvedora Back-end - Node JS",
     enterprise: "Brazilians in Tech",
     location: "Rio de Janeiro",
     description:
@@ -19,9 +19,36 @@ const jobsData = [
   },
   {
     id: 2,
-    title: "Pessoa desenvolvedora Fullstack - Node JS",
+    title: "Desenvolvedora Fullstack - Java",
+    enterprise: "IBM",
+    location: "São Paulo",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    modalities: ["Híbrido", "Presencial"],
+  },
+  {
+    id: 3,
+    title: "Desenvolvedora Fullstack - Node JS",
     enterprise: "Brazilians in Tech",
     location: "Rio de Janeiro",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    modalities: ["Home Office", "Presencial"],
+  },
+  {
+    id: 4,
+    title: "Desenvolvedora Back-End - Python",
+    enterprise: "Meta",
+    location: "São Paulo",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    modalities: ["Home Office", "Presencial"],
+  },
+  {
+    id: 5,
+    title: "Desenvolvedora Front-End - React",
+    enterprise: "Madeira Madeira",
+    location: "São Paulo",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
     modalities: ["Home Office", "Presencial"],
